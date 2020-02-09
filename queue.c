5717fe1275c0dc1002657f966af0eb4bf5968073
OscarShiang <oscar0225@livemal.tw>
2020-02-09 19:38:35 +0800

    Copy string into new memory

--- queue.c
+++ queue.c
@@ -14,6 +14,7 @@ queue_t *q_new()
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
     q->head = NULL;
+    q->size = 0;
     return q;
 }
 
@@ -38,10 +39,27 @@ bool q_insert_head(queue_t *q, char *s)
     list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+
+    // connect the link
+    if (!q) {
+        q = q_new();
+    }
     newh->next = q->head;
     q->head = newh;
+
+    // copy the string
+    int str_len = strlen(s);
+    char *str = malloc(str_len + 1);
+    strncpy(str, s, str_len);
+    str[str_len] = '\0';
+    newh->value = str;
+
+    size++;
+
     return true;
 }
 
@@ -85,7 +103,7 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    if (q == NULL)
+    if (q == NULL || q->head == NULL)
         return 0;
     else
         return q->size;
